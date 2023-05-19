adc60ba3a3e9d6fa0f65bc2eb7652b9c11d64c14
kgjaif <102163974+kgjaif@users.noreply.github.com>
2023-05-19 16:54:58 +0800

    Update socket_ds18b20.c

--- client/socket_ds18b20.c
+++ client/socket_ds18b20.c
@@ -155,7 +155,7 @@ int main(int argc, char **argv)
 	}
 
 	memset(&hints,0,sizeof(hints));
-	hints.ai_flags-=AI_PASSIVE;
+	hints.ai_flags=AI_PASSIVE;
 	hints.ai_socktype=SOCK_STREAM;
 	hints.ai_family=AF_INET;
 	hints.ai_protocol=0;
